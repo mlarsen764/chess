cc8451494057d71f4311197438d62162016338f4
Matt Larsen <mattman6@byu.edu>
2024-09-30 09:44:48 -0600

    add tests, add getter and setter for team color

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -9,16 +9,16 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
-
+    private TeamColor teamTurn;
     public ChessGame() {
-
+        this.teamTurn = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +27,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.teamTurn = team;
     }
 
     /**
@@ -46,6 +46,7 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        // return moves that don't leave your king in check
         throw new RuntimeException("Not implemented");
     }
 
@@ -66,6 +67,7 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
+        // iterate over opposing player's moves to see if any of the end positions include the king's position.
         throw new RuntimeException("Not implemented");
     }
 
@@ -76,6 +78,8 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
+        // if you're not in check, you're not in checkmate. Apply moves and call isincheck for each one.
+        // Iterate the moves, copy the board (getBoard().clone), make the move, call isincheck. If any get out of check, not in checkmate
         throw new RuntimeException("Not implemented");
     }
 
