8982a43c31274b32334a9474a2bab8bd031e89ac
mlarsen764 <mlarsen764@gmail.com>
2024-10-29 16:28:17 -0600

    implement methods for sql user dao

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,19 +2,51 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SQLUserDAO implements UserDAO {
     @Override
     public void clear() throws DataAccessException {
-
+        String sql = "DELETE FROM Users";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error clearing users");
+        }
     }
 
     @Override
     public void createUser(UserData u) throws DataAccessException {
-        String sql = ""
+        String sql = "INSERT INTO Users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, u.username());
+            stmt.setString(2, u.password());
+            stmt.setString(3, u.email());
+        } catch (SQLException e) {
+            throw new DataAccessException("Error creating user:" + u.username());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        String sql = "SELECT * FROM Users WHERE username = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"),
+                                    rs.getString("password"),
+                                    rs.getString("email"));
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DataAccessException("Error retrieving user");
+        }
     }
 }
