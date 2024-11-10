784a1c4b7f0698e7a3014eb2c4921608c32f9e74
mlarsen764 <mlarsen764@gmail.com>
2024-11-09 21:04:15 -0700

    add game methods to server facade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -3,9 +3,12 @@ package client;
 import com.google.gson.Gson;
 import chess.*;
 import model.*;
+import requests.JoinGameRequest;
 
 import java.io.*;
 import java.net.*;
+import java.util.Collection;
+import java.util.List;
 
 public class ServerFacade {
 
@@ -15,34 +18,67 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    public AuthData registerUser(UserData user) throws Exception {
+    public AuthData register(UserData user) throws Exception {
         String path = "/register";
-        return this.makeRequest("POST", path, user, AuthData.class);
+        return this.makeRequest(path, user, AuthData.class);
     }
 
-    public AuthData loginUser(UserData user) throws Exception {
+    public AuthData login(UserData user) throws Exception {
         String path = "/login";
-        return this.makeRequest("POST", path, user, AuthData.class);
+        return this.makeRequest(path, user, AuthData.class);
     }
 
-    public ChessGame createGame(ChessGame game) throws Exception {
+    public void logout(String authToken) throws Exception {
+        String path = "/logout";
+        this.makeRequest(path, new AuthData(authToken, null), null);
+    }
+
+    public GameData createGame(String authToken, GameData gameData) throws Exception {
         String path = "/game";
-        return this.makeRequest("POST", path, game, ChessGame.class);
+        return this.makeRequestWithAuth("POST", path, authToken, gameData, GameData.class);
     }
 
-    public ChessGame[] listGames() throws Exception {
+    public void joinGame(String authToken, int gameID, String playerColor, ChessGame chessGame) throws Exception {
+        String path = String.format("/game/%d/join", gameID);
+        var request = new JoinGameRequest(authToken, playerColor, gameID);
+        this.makeRequestWithAuth("POST", path, authToken, request, null);
+    }
+
+    public Collection<GameData> listGames(String authToken) throws Exception {
         String path = "/games";
-        record ListGamesResponse(ChessGame[] games) {}
-        var response = this.makeRequest("GET", path, null, ListGamesResponse.class);
-        return response.games();
+        record listGamesResponse(GameData[] game) {
+        }
+        var response = this.makeRequestWithAuth("GET", path, authToken, null, listGameResponse.class);
+        return List.of(response.game());
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
+
+    private <T> T makeRequest(String path, Object request, Class<T> responseClass) throws Exception {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new Exception();
+        }
+    }
+
+    private <T> T makeRequestWithAuth(String method, String path, String authToken, Object request, Class<T> responseClass) throws Exception {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
             http.setDoOutput(true);
+
+            if (authToken != null && !authToken.isEmpty()) {
+                http.setRequestProperty("Authorization", "Bearer " + authToken);
+            }
 
             writeBody(request, http);
             http.connect();
